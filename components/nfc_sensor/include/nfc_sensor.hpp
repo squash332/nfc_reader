b2b2b2d248efbd88b69e957061d3177f153c0560
squash332 <filip.hulak12@gmail.com>
2026-04-10 11:36:39 +0200

    nfc skeleton

--- components/nfc_sensor/include/nfc_sensor.hpp
+++ components/nfc_sensor/include/nfc_sensor.hpp
@@ -1,14 +1,14 @@
 #pragma once
 
 #include "pn532_driver_spi.h"
+#include "esp_log.h"
 
 
 class PN532 {
     public:
-        PN532() = default;
-        ~PN532() = default;
-
-        esp_err_t spi_init();
+        PN532();
+        ~PN532();
+        esp_err_t init_module_and_bus();
 
     private:
         pn532_io_handle_t io_handle;
--- components/nfc_sensor/nfc_sensor.cpp
+++ components/nfc_sensor/nfc_sensor.cpp
@@ -1,9 +1,14 @@
 #include "nfc_sensor.hpp"
 #include "board_spi_gpio.hpp"
 
-esp_err_t PN532::spi_init()
+PN532::PN532()
 {
-    return pn532_new_driver_spi(
+    io_handle = nullptr;
+}
+
+esp_err_t PN532::init_module_and_bus()
+{
+    esp_err_t ret = pn532_new_driver_spi(
         NFC_MISO,
         NFC_MOSI,
         NFC_SCK,
@@ -13,4 +18,22 @@ esp_err_t PN532::spi_init()
         NFC_SPI_HOST,
         NFC_CLOCK_FREQ,
         io_handle);
+    if (ret != ESP_OK)
+        return ret;
+
+    ret = pn532_init(io_handle);
+    if (ret != ESP_OK)
+        return ret;
+
+    return ESP_OK;
+}
+
+PN532::~PN532()
+{
+    if (!io_handle) return;
+
+    pn532_release(io_handle);
+    pn532_delete_driver(io_handle);
+
+    io_handle = nullptr;
 }
